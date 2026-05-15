8ed2ca4788dbe41865209be1c9b93e5226bd6289
Hamza <hamza@student.be>
2026-05-15 13:17:00 +0200

    api.js: foutafhandeling met response.ok

--- src/js/api.js
+++ src/js/api.js
@@ -4,7 +4,13 @@ const API_URL = "https://dragonball-api.com/api/characters?limit=25";
 
 async function haalCharactersOp() {
   const response = await fetch(API_URL);
+
+  if (!response.ok) {
+    throw new Error("API werkt niet: " + response.status);
+  }
+
   const data = await response.json();
+  console.log("API data ontvangen:", data);
   return data.items;
 }
 
